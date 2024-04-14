447aad93508ad04a39763b5a503c4968b11aa8b8
Thomas <thomasribalta@gmail.com>
2024-04-14 13:13:54 +0200

    add thread for philosophe

--- Mandatory/main.c
+++ Mandatory/main.c
@@ -13,21 +13,57 @@ static int check_args(char **args)
   return (1);
 }
 
-static void init_global_info(t_info *info, int ac, char **av)
+static void init_global_info(t_global **global, int ac, char **av)
 {
-  info->n_philo = ft_atoi(av[1]);
-  info->time_die = ft_atoi(av[2]);
-  info->time_eat = ft_atoi(av[3]);
-  info->time_sleep = ft_atoi(av[4]);
+  t_info info;
+
+  info.n_philo = ft_atoi(av[1]);
+  info.time_die = ft_atoi(av[2]);
+  info.time_eat = ft_atoi(av[3]);
+  info.time_sleep = ft_atoi(av[4]);
   if (ac == 6)
-    info->eat_interval = ft_atoi(av[5]);
+    info.eat_interval = ft_atoi(av[5]);
+  (*global)->info = info;
   }
+
+static void *test(){
+  printf("test prev\n");
+  usleep(10000000);
+  printf("test\n");
+  return (NULL);
+}
+
+static void create_philosophers(t_global **global){
+  int i;
+
+  i = 0;
+  (*global)->philos = malloc(sizeof(t_philo) * (*global)->info.n_philo);
+  if (!(*global)->philos)
+      return;
+  (*global)->philos_threads = malloc(sizeof(pthread_t) * (*global)->info.n_philo);
+  if (!(*global)->philos_threads)
+      return (free((*global)->philos));
+  while (i++ != (*global)->info.n_philo) {
+      (*global)->philos[i].is_dead = 0;
+      (*global)->philos[i].fork = malloc(sizeof(pthread_mutex_t));
+      if (!(*global)->philos[i].fork)
+        return (free((*global)->philos));
+      pthread_mutex_init((*global)->philos[i].fork, NULL);
+      pthread_create(&((*global)->philos_threads[i]), NULL, &test, NULL);
+  }
+  i = 0;
+  while (i++ != (*global)->info.n_philo)
+      pthread_join((*global)->philos_threads[i], NULL);
+}
 
 static void start(int ac, char **av)
 {
-  t_info info;
-  init_global_info(&info, ac, av);
-  printf("%d", info.n_philo);
+  t_global *global;
+  global = malloc(sizeof(t_global));
+  if (!global)
+    return((void) printf("erreur malloc"), (void) 0);
+  init_global_info(&global, ac, av);
+  create_philosophers(&global);
 }
 
 int main(int ac, char **av)
--- Mandatory/philosopher.h
+++ Mandatory/philosopher.h
@@ -1,8 +1,11 @@
 #ifndef PHILOSOPHER
 # define PHILOSOPHER
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
 
 typedef struct s_info
 {
@@ -12,6 +15,20 @@ typedef struct s_info
   int time_sleep;
   int eat_interval;
 } t_info;
+
+typedef struct s_philo
+{
+  pthread_mutex_t	*fork;
+  int is_dead;
+} t_philo;
+
+typedef struct s_global
+{
+  t_info info;
+  t_philo *philos;
+  pthread_t *philos_threads;
+
+} t_global;
 
 
 int	ft_atoi(const char *str);
